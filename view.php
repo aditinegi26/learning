399492a520113a68b7f588f83e374173297c6a1b
aditinegi26 <aditinegi26@gmail.com>
2020-10-21 13:12:53 +0530

    text box and alert added

---
+++ view.php
@@ -0,0 +1,11 @@
+<div>
+  <label for="qty">Quantity:</label>
+  <input type="text" name="qty" id="qty" onmouseout="javascript:updateQty();">
+</div>
+
+<script>
+function updateQty()
+{
+  alert('qty box clicked');
+}
+</script>
